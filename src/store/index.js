f461ed40c8973c4c2398a01ee2c9836afdda648b
Chris Miles <mail@christophermiles.co.uk>
2020-06-03 13:40:04 +0100

    chore: Only allow redux devtools in development mode

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers/";
 import thunk from "redux-thunk";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
 export default createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
